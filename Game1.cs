2ac1e79248cb44517c33aaed39b72ec9eaba5df9
n00bCod3r <n00bCod3r@protonmail.com>
2023-03-02 21:47:08 +0100

    updates
    Add fruit sprite, set its initial position, add timer to change its position every 5 secs, change background color, add regions

--- Game1.cs
+++ Game1.cs
@@ -1,52 +1,92 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 
-namespace snek
-{
-	public class Game1 : Game
-	{
-		private GraphicsDeviceManager _graphics;
-		private SpriteBatch _spriteBatch;
+namespace snek {
+	public class Game1 : Game	{
+		#region Attributes
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+		private Texture2D fruitTexture;
+		private Texture2D snakeHeadTexture;
+		private Vector2 fruitPosition;
 
-		public Game1()
-		{
-			_graphics = new GraphicsDeviceManager(this);
+		// Timer
+		readonly float countDuration; 
+		float currentTime;
+		#endregion
+
+		// Constructor, used to initialize the starting variables
+		public Game1() {
+			this.graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
-			IsMouseVisible = true;
+			this.IsMouseVisible = true;
+			this.countDuration = 5f;
+			this.currentTime = 0f;
 		}
 
-		protected override void Initialize()
-		{
-			// TODO: Add your initialization logic here
+		#region BaseFunctions
+		// Called after the constructor, used to query any required services and load any non-graphic related content
+		protected override void Initialize() {
+			Debug.WriteLine($"Width: {this.graphics.PreferredBackBufferHeight}, Height: {this.graphics.PreferredBackBufferWidth}");
 
 			base.Initialize();
 		}
 
-		protected override void LoadContent()
-		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+		// Called within the Initialize method, used to load game content 
+		protected override void LoadContent() {
+			this.spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// TODO: use this.Content to load your game content here
+			this.fruitTexture = Content.Load<Texture2D>("fruit");
+			this.fruitPosition = this.GetXYPosition(fruitTexture.Width, fruitTexture.Height);
+			this.snakeHeadTexture = Content.Load<Texture2D>("snakeHead");
 		}
 
-		protected override void Update(GameTime gameTime)
-		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-				Exit();
+		// Called multiple times per second, draws content to the screen
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.Black);
 
-			// TODO: Add your update logic here
-
-			base.Update(gameTime);
-		}
-
-		protected override void Draw(GameTime gameTime)
-		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
-
-			// TODO: Add your drawing code here
+			this.spriteBatch.Begin();
+			this.spriteBatch.Draw(fruitTexture, fruitPosition, Color.White);
+			this.spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
+
+		// Called multiple times per second, updates the game state
+		protected override void Update(GameTime gameTime)	{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			#region timer actions
+			// Calculate time passed since last Update() 
+			this.currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds; 
+
+			if (this.currentTime >= this.countDuration)	{
+				this.currentTime -= this.countDuration; // reset timer
+				this.fruitPosition = GetXYPosition(fruitTexture.Width, fruitTexture.Height); //update fruit position
+			}
+			#endregion
+
+			base.Update(gameTime);
+		}
+		#endregion
+
+		#region MyFunctions
+		private Vector2 GetXYPosition(int width, int height) {
+			int lowerBound = 0;
+			int upperBoundX = this.graphics.PreferredBackBufferWidth - width;
+			int upperBoundY = this.graphics.PreferredBackBufferHeight - height;
+			Random random = new();
+			int posX = random.Next(lowerBound, upperBoundX);
+			int posY = random.Next(lowerBound, upperBoundY);
+
+			return new Vector2(posX, posY);
+		}
+		#endregion
 	}
 }
